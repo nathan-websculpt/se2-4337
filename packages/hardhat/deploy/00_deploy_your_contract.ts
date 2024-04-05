5db558e4d2b5b90c55516747d09d8557c4b6f1a9
nathan-websculpt <websculpt.nathan@gmail.com>
2024-04-05 11:40:31 -0500

    deployed new contract that has some non-payable functions

--- packages/hardhat/deploy/00_deploy_your_contract.ts
+++ packages/hardhat/deploy/00_deploy_your_contract.ts
@@ -25,7 +25,7 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
   await deploy("YourContract", {
     from: deployer,
     // Contract constructor arguments
-    args: [deployer],
+    args: ["0x1e7aAbB9D0C701208E875131d0A1cFcDAba79350"],
     log: true,
     // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
     // automatically mining the contract deployment transaction. There is no effect on live networks.
